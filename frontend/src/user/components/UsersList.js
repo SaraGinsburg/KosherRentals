9cf903982da79733ee89754b592e81ee3f27dc55
SARA GINSBURG <sara.ginsburg.ny@gmail.com>
2021-12-14 22:03:01 -0500

    add UserItem component

--- frontend/src/user/components/UsersList.js
+++ frontend/src/user/components/UsersList.js
@@ -1,24 +1,25 @@
-import React from ('react')
-import ('./UsersList.css')
-const UsersList = props =>
-{
+import React from 'react'
+import UserItem from './UserItem'
+
+import './UsersList.css'
+const UsersList = (props) => {
   if (props.items.length === 0) {
-    return <div className='center'>
-      "No Users Found"
-    </div>
+    return <div className='center'>No Users Found</div>
   }
   return (
-    <ul>
-      {props.items.map((user) => {
+    <ul className='users-list'>
+      {props.items.map((user) => (
         <UserItem
           key={user.id}
           id={user.id}
           image={user.image}
           name={user.name}
-          placesCount={user.places} />
-      })}
+          placesCount={user.places}
+        />
+      ))}
+      )
     </ul>
+  )
+}
 
-  )  }
-
-export default UsersList;
+export default UsersList
